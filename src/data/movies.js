b9f74fbe67ea49162fb7ac2e2cbab5a5d38ab831
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-26 14:48:02 -0300

    Search

--- src/data/movies.js
+++ src/data/movies.js
@@ -2,6 +2,8 @@ export const movies = [
   {
     id: 1,
     title: "Breaking Bad",
+    description:
+      "Un profesor de química convertido en fabricante de metanfetaminas se adentra en el mundo del crimen para asegurar el futuro de su familia.",
     genre: "Drama",
     year: 2008,
     image: "https://picsum.photos/400/600?random=1",
@@ -18,6 +20,8 @@ export const movies = [
   {
     id: 3,
     title: "The Batman",
+    description:
+      "El Caballero Oscuro regresa a las calles de Gotham para enfrentar a nuevos villanos y proteger a su ciudad.",
     genre: "Acción",
     year: 2022,
     image: "https://picsum.photos/400/600?random=3",
@@ -26,6 +30,8 @@ export const movies = [
   {
     id: 4,
     title: "The Crown",
+    description:
+      "Una mirada íntima a la vida de la Reina Isabel II, explorando los desafíos y triunfos de su reinado a lo largo de las décadas.",
     genre: "Drama histórico",
     year: 2016,
     image: "https://picsum.photos/400/600?random=4",
@@ -34,6 +40,8 @@ export const movies = [
   {
     id: 5,
     title: "The Witcher",
+    description:
+      "Geralt de Rivia, un cazador de monstruos solitario, lucha por encontrar su lugar en un mundo donde las personas a menudo son más malvadas que las bestias.",
     genre: "Fantasía",
     year: 2019,
     image: "https://picsum.photos/400/600?random=5",
@@ -42,6 +50,8 @@ export const movies = [
   {
     id: 6,
     title: "Interstellar",
+    description:
+      "Un grupo de exploradores viaja a través de un agujero de gusano en el espacio en un intento por asegurar la supervivencia de la humanidad.",
     genre: "Ciencia ficción",
     year: 2014,
     image: "https://picsum.photos/400/600?random=6",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import { movies } from "../data/movies";
 import MovieList from "../components/MovieList";
+import { useState } from "react";
 
 function Home() {
+  const [search, setSearch] = useState("");
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase()) ||
+      movie?.description?.toLowerCase().includes(search.toLowerCase()),
+  );
+
   const featuredMovies = movies.filter((movie) => movie.featured);
 
   const newMovies = movies.slice(0, 3); // 3 primeras
+
+  const hasResults = filteredMovies.length > 0;
 
   return (
     <main>
@@ -36,6 +47,36 @@ function Home() {
           <h2>Nuevas peliculas</h2>
 
           <MovieList movies={newMovies} />
+        </div>
+      </section>
+
+      <section className="catalog-section">
+        <div className="container">
+          <div className="section-header">
+            <h2>Explorar catálogo</h2>
+            <p>Busca películas y series por título y descripción.</p>
+          </div>
+
+          <label className="search-label" htmlFor="search">
+            Buscar por titulo o descripción:
+          </label>
+          <input
+            className="search-input"
+            placeholder="Buscar por titulo o descripción..."
+            type="text"
+            name="search"
+            id="search"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+
+          {hasResults ? (
+            <MovieList movies={filteredMovies} />
+          ) : (
+            <p className="empty-message">
+              No encontramos resultados para la busqueda
+            </p>
+          )}
         </div>
       </section>
     </main>
