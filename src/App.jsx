fa4238926b5d57c5f52526d7c66f9f980f1fd71e
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-27 14:25:17 -0300

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
