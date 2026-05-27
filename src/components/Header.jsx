99f360ba630cb63a78303909843c4fd36095daad
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-27 16:59:07 -0300

    MovieDetailPage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./Navbar";
+import logo from "../assets/favicon.svg";
 
 function Header() {
   return (
     <header className="site-header">
       <div className="header-content container">
-        <div className="header-item">ScreenHub</div>
+        <div className="header-item">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
         <Navbar />
       </div>
     </header>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import MoviesPage from "../pages/MoviesPage";
+import MovieDetailPage from "../pages/MovieDetailPage";
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
       {
         path: "/movies",
         element: <MoviesPage />,
+      },
+      {
+        path: "/movies/:id",
+        element: <MovieDetailPage />,
       },
     ],
   },
