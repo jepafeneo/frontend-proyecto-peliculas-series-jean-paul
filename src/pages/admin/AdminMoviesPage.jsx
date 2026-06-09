02eb553fd3c13a7a5c5a71288eb1cfb673f842fa
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-06-09 17:20:51 -0300

    SearchBox

--- src/pages/admin/AdminMoviesPage.jsx
+++ src/pages/admin/AdminMoviesPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useRef } from "react";
-// import { movies as initialMovies } from "../../data/movies";
 import MovieForm from "../../components/MovieForm";
 import {
   createMovie,
